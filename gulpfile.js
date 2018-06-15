2b35c0fb98bd48ad07cea5a27dc4fdb6b0609b64
YudhaYuki <YudhaYuki@hotmail.com>
2018-06-15 12:34:56 +0200

    Large Hero Block

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+
+
 
 gulp.task('default', function() {
     console.log('Horay !!!')
@@ -10,7 +18,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-    console.log('Manipuated SASS or CSS')
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
